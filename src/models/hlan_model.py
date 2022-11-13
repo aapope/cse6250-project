39a788cc7945da695e6a3c1a4e030edc50f45f8b
Andrew Pope <andrew@andrewapope.com>
2022-11-13 13:31:48 -0700

    training script and initialization using label embeddings

--- src/models/hlan_model.py
+++ src/models/hlan_model.py
@@ -1,17 +1,22 @@
+import math
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 
 class HLAN(nn.Module):
-    def __init__(self, max_document_length=100, sentence_length=25, embedding_length=100, num_labels=50, word_level_hidden_size=100, sentence_level_hidden_size=200):
+    def __init__(self, max_document_length=100, sentence_length=25, embedding_length=100, num_labels=50, word_level_hidden_size=100, sentence_level_hidden_size=200, use_sentence_attention_per_label=True, use_word_attention_per_label=False, label_embeddings=None):
         super(HLAN, self).__init__()
         
         self.max_document_length = max_document_length
         self.sentence_length = sentence_length
         self.embedding_length = embedding_length
+        self.num_labels = num_labels
         self.word_level_hidden_size = word_level_hidden_size
         self.sentence_level_hidden_size = sentence_level_hidden_size
+        self.use_sentence_attention_per_label = use_sentence_attention_per_label
+        self.use_word_attention_per_label = use_word_attention_per_label
 
         self.word_level_gru = nn.GRU(
             embedding_length, word_level_hidden_size, num_layers=1, batch_first=True,
@@ -21,65 +26,112 @@ class HLAN(nn.Module):
             2 * word_level_hidden_size,
             2 * word_level_hidden_size
         )
-        # TODO: initialize the below using the lbel embeddings
-        self.context_vector_word = nn.Parameter(torch.ones(self.word_level_hidden_size * 2))
 
-        self.sentence_level_gru = nn.GRU(
-            2 * word_level_hidden_size, sentence_level_hidden_size, num_layers=1, batch_first=True,
-            bidirectional=True
-        )
-        self.sentence_level_attention = nn.Linear(
-            2 * sentence_level_hidden_size,
-            2 * sentence_level_hidden_size
-        )
+        # TODO: initialize the below using the label embeddings
+        if use_word_attention_per_label:
+            self.context_vector_word = nn.Parameter(torch.ones(num_labels, word_level_hidden_size * 2))
+            # TODO: re-implement GRU for word-level attention
+        else:
+            self.context_vector_word = nn.Parameter(torch.ones(self.word_level_hidden_size * 2))
+
+            self.sentence_level_gru = nn.GRU(
+                2 * word_level_hidden_size, sentence_level_hidden_size, num_layers=1, batch_first=True,
+                bidirectional=True
+            )
+            self.sentence_level_attention = nn.Linear(
+                2 * sentence_level_hidden_size,
+                2 * sentence_level_hidden_size
+            )
+
+        
         # TODO: initialize the below using label embeddings
-        self.context_vector_sentence = nn.Parameter(torch.ones(2 * self.sentence_level_hidden_size))
+        if use_sentence_attention_per_label:
+            self.context_vector_sentence = nn.Parameter(torch.ones(num_labels, 2 * self.sentence_level_hidden_size))
 
-        self.output_layer = nn.Linear(
-            2 * sentence_level_hidden_size,
-            num_labels
-        )
+            # initialize weights the same way that nn.Linear would
+            # https://pytorch.org/docs/stable/generated/torch.nn.Linear.html
+            bound = math.sqrt(1 / (2 * self.sentence_level_hidden_size))
+            if label_embeddings is not None:
+                print('Using label embeddings to initialize attention matrix')
+                initial = torch.Tensor(label_embeddings).float()
+                assert initial.shape == (2 * self.sentence_level_hidden_size, num_labels)
+                self.W_output = nn.Parameter(initial)
+            else:
+                print('NOT USING label embeddings to initialize attention matrix.')
+                self.W_output = nn.Parameter(
+                    torch.FloatTensor(2 * self.sentence_level_hidden_size, num_labels).uniform_(-bound, bound)
+                )
+            self.b_output = nn.Parameter(
+                torch.FloatTensor(num_labels).uniform_(-bound, bound)
+            )
+        else:
+            self.context_vector_sentence = nn.Parameter(torch.ones(2 * self.sentence_level_hidden_size))
+            self.output_layer = nn.Linear(
+                2 * sentence_level_hidden_size,
+                num_labels
+            )
 
     def forward(self, x):
         data, document_lengths, sentence_lengths = x
         document_length = document_lengths.max().item()
         # data is of shape (batch_size, num_sentences, sentence_length, embed_length)
-        print(f'Input data shape: {data.shape}')
+        # print(f'Input data shape: {data.shape}')
 
         # reshape to (batch_size * num_sentences, sentence_length, embed_length)
         words = data.reshape(-1, self.sentence_length, self.embedding_length)
-        print(f'Shape into Word GRU: {words.shape}')
+        # print(f'Shape into Word GRU: {words.shape}')
 
         # shape: (batch_size * num_sentences, sentence_length, 2 * hidden)
         gru_output, _ = self.word_level_gru(words)
-        print(f'Shape of Word GRU output: {gru_output.shape}')
+        # print(f'Shape of Word GRU output: {gru_output.shape}')
 
-        # shape: (batch_size, num_sentences, 2 * hidden)
-        sentences = self.word_attention(gru_output)
-        sentences = sentences.reshape(
-            -1, document_length, 2 * self.word_level_hidden_size
-        )
-        print(f'Shape of word attention output: {sentences.shape}')
+        if self.use_word_attention_per_label:
+            sentences = self.word_attention_per_label(gru_output)
+            sentences = sentences.reshape(
+                self.num_labels, -1, document_length, 2 * self.word_level_hidden_size
+            )
+            # print(f'Shape of word attention output: {sentences.shape}')
+            
+            # TODO: word attention per-label will require a GRU re-implementation
+            # to support 4-dimensional inputs. I searched around to see if there's
+            # an implementation available, with no luck
 
-        # shape (batch_size, num_sentences, 2 * sentence_hidden)
-        gru_output, _ = self.sentence_level_gru(sentences)
-        print(f'Shape of Sentence GRU output: {gru_output.shape}')
+        else:
+            # shape: (batch_size, num_sentences, 2 * hidden)
+            sentences = self.word_attention(gru_output)
+            sentences = sentences.reshape(
+                -1, document_length, 2 * self.word_level_hidden_size
+            )
+            # print(f'Shape of word attention output: {sentences.shape}')
 
-        documents = self.sentence_attention(gru_output, document_length)
-        output = self.output_layer(documents)
-        print(output.shape)
-        return output
+            # shape (batch_size, num_sentences, 2 * sentence_hidden)
+            gru_output, _ = self.sentence_level_gru(sentences)
+            # print(f'Shape of Sentence GRU output: {gru_output.shape}')
+
+        if self.use_sentence_attention_per_label:
+            documents = self.sentence_attention_per_label(gru_output, document_length)
+            # TODO: the paper's code uses dropout here. we might want to try that
+            documents = documents.permute(dims=(1, 2, 0))
+            # print(documents.shape)
+
+            logits = torch.mul(documents, self.W_output).sum(dim=1) + self.b_output
+        else:
+            documents = self.sentence_attention(gru_output, document_length)
+            logits = self.output_layer(documents)
+            
+        # print(logits.shape)
+        return logits
 
 
     def sentence_attention(self, hidden_input, document_length):
         hidden_input_reshaped = hidden_input.reshape(-1, 2 * self.sentence_level_hidden_size)
-        print(hidden_input_reshaped.shape)
+        # print(hidden_input_reshaped.shape)
 
         sentence_level_attention = torch.tanh(self.sentence_level_attention(hidden_input_reshaped))
         # todo: in the paper's code, this is reshaped to (-1, doc length, sent level hidden), which
         # results in duplicating the batch size. I couldn't get it to work that way.
         sentence_level_attention = sentence_level_attention.reshape(-1, document_length, 2 * self.sentence_level_hidden_size)
-        print(sentence_level_attention.shape)
+        # print(sentence_level_attention.shape)
 
         x = torch.mul(sentence_level_attention, self.context_vector_sentence)
         attention_logits = torch.sum(x, dim=2)
@@ -92,51 +144,119 @@ class HLAN(nn.Module):
             torch.mul(p_attention_expanded, hidden_input),
             dim=1
         )
-        print(document_representation.shape)
+        # print(document_representation.shape)
         return document_representation
         
 
     def word_attention(self, hidden_input):
         # reshape to (batch_size * num_sentences * sentence_length, 2 * hidden)
         hidden_input_reshaped = hidden_input.reshape(-1, 2 * self.word_level_hidden_size)
-        print(f'Shape of input to word attention: {hidden_input.shape}')
+        # print(f'Shape of input to word attention: {hidden_input.shape}')
 
         # same shape as above: (batch_size * num_sentences * sentence_length, 2 * hidden)
         word_level_attention = torch.tanh(self.word_level_attention(hidden_input_reshaped))
-        print(f'Shape of word level attention output: {word_level_attention.shape}')
+        # print(f'Shape of word level attention output: {word_level_attention.shape}')
 
         # back to (batch_size * num_sentences, sentence_length, 2 * hidden)
         x = word_level_attention.reshape(-1, self.sentence_length, 2 * self.word_level_hidden_size)
-        print(x.shape)
+        # print(x.shape)
 
         # (batch_size * num_sentences, sentence_length, 2 * hidden)
         x = torch.mul(x, self.context_vector_word)
-        print(x.shape)
+        # print(x.shape)
 
         # (batch_size * num_sentences, sentence_length)
         attention_logits = torch.sum(x, dim=2)
-        print(attention_logits.shape)
+        # print(attention_logits.shape)
 
         # (batch_size * num_sentences, 1)
         attention_logits_max, _ = torch.max(attention_logits, dim=1, keepdim=True)
-        print(attention_logits_max.shape)
+        # print(attention_logits_max.shape)
 
         # (batch_size * num_sentences, sentence_length)
         p_attention = F.softmax(attention_logits - attention_logits_max)
-        print(p_attention.shape)
+        # print(p_attention.shape)
 
         # (batch_size * num_sentences, sentence_length, 1)
         p_attention_expanded = torch.unsqueeze(p_attention, dim=-1)
-        print(p_attention_expanded.shape)
+        # print(p_attention_expanded.shape)
 
         # (batch_size * num_sentences, 2 * hidden)
         sentence_representation = torch.sum(
             torch.mul(p_attention_expanded, hidden_input),
             dim=1
         )
-        print(sentence_representation.shape)
+        # print(sentence_representation.shape)
 
         return sentence_representation
         
         
+    def word_attention_per_label(self, hidden_input):
+        # reshape to (batch_size * num_sentences * sentence_length, 2 * hidden)
+        hidden_input_reshaped = hidden_input.reshape(-1, 2 * self.word_level_hidden_size)
+        # print(f'Shape of input to word attention: {hidden_input.shape}')
+
+        # same shape as above: (batch_size * num_sentences * sentence_length, 2 * hidden)
+        word_level_attention = torch.tanh(self.word_level_attention(hidden_input_reshaped))
+        # print(f'Shape of word level attention output: {word_level_attention.shape}')
+
+        # back to (batch_size * num_sentences, sentence_length, 2 * hidden)
+        x = word_level_attention.reshape(-1, self.sentence_length, 2 * self.word_level_hidden_size)
+        # print(x.shape)
+
+        x = x.unsqueeze(dim=0)
+        # print(f'After reshape: {x.shape}')
+
+        expanded_context_vector = self.context_vector_word.unsqueeze(dim=1).unsqueeze(dim=1)
+        # print(f'Context vector: {expanded_context_vector.shape}')
+
+        x = torch.mul(x, expanded_context_vector)
+        # print(f'Similarity: {x.shape}')
+        
+        attention_logits = x.sum(dim=-1)
+        # print(f'After sum: {attention_logits.shape}')
+
+        attention_logits_max, _ = attention_logits.max(dim=-1, keepdim=True)
+        # print(attention_logits_max.shape)
+
+        p_attention = F.softmax(attention_logits - attention_logits_max)
+        p_attention_expanded = p_attention.unsqueeze(dim=-1)
+        # print(p_attention_expanded.shape)
+
+        sentence_representation = torch.sum(
+            torch.mul(p_attention_expanded, hidden_input),
+            dim=2
+        )
+        # print(sentence_representation.shape)
+
+        return sentence_representation
+
+    def sentence_attention_per_label(self, hidden_input, document_length):
+        hidden_input_reshaped = hidden_input.reshape(-1, 2 * self.sentence_level_hidden_size)
+        # print(hidden_input_reshaped.shape)
+
+        sentence_level_attention = torch.tanh(self.sentence_level_attention(hidden_input_reshaped))
+        sentence_level_attention = sentence_level_attention.reshape(-1, document_length, 2 * self.sentence_level_hidden_size)
+        sentence_level_attention = sentence_level_attention.unsqueeze(dim=0)
+        # print(sentence_level_attention.shape)
+
+        expanded_context_vector = self.context_vector_sentence.unsqueeze(dim=1).unsqueeze(dim=1)
+        # print(f'Context vector: {expanded_context_vector.shape}')
+
+        x = torch.mul(sentence_level_attention, expanded_context_vector)
+        # print(f'Similarity: {x.shape}')
+
+        attention_logits = x.sum(dim=-1)
+        attention_logits_max, _ = torch.max(attention_logits, dim=-1, keepdim=True)
+        p_attention = F.softmax(attention_logits - attention_logits_max)
+        p_attention_expanded = p_attention.unsqueeze(dim=-1)
+        # print(p_attention_expanded.shape)
+
+        document_representation = torch.sum(
+            torch.mul(p_attention_expanded, hidden_input),
+            dim=2
+        )
+        # print(document_representation.shape)
+
+        return document_representation
         
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -3,15 +3,26 @@ import logging
 import os
 import sys
 from pprint import pprint
+import time
 
 import torch
-from torch.utils.data import Dataset
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+import torch.optim as optim
 import numpy as np
+from gensim.models import Word2Vec
 
 from hlan_model import HLAN
 
 
 PROCESSED_DATA_PATH = '../../data/processed'
+MODEL_OUTPUT_PATH = '../../models/HLANModel.pth'
+
+NUM_EPOCHS = 30
+BATCH_SIZE = 32
+USE_CUDA = True
+
+
 logging.basicConfig(
     format="%(levelname)s - %(asctime)s - %(filename)s - %(message)s",
     level=logging.INFO,
@@ -43,6 +54,25 @@ class HLANDataset(Dataset):
 
     def __getitem__(self, index):
         return self.sequence[index], self.labels[index], self.doc_lengths[index], self.sentence_lengths[index]
+
+
+class AverageMeter(object):
+	"""Computes and stores the average and current value"""
+
+	def __init__(self):
+		self.reset()
+
+	def reset(self):
+		self.val = 0
+		self.avg = 0
+		self.sum = 0
+		self.count = 0
+
+	def update(self, val, n=1):
+		self.val = val
+		self.sum += val * n
+		self.count += n
+		self.avg = self.sum / self.count
 
 
 def document_collate_function(batch):
@@ -109,7 +139,7 @@ def document_collate_function(batch):
     data_tensor = torch.tensor(np.array(data)).float()
     doc_lengths_tensor = torch.tensor(doc_lengths).long()
     sent_lengths_tensor = torch.tensor(np.array(sentence_lengths)).long()
-    label_tensor = torch.tensor(labels).long()
+    label_tensor = torch.tensor(labels).float()
     
     return (data_tensor, doc_lengths_tensor, sent_lengths_tensor), label_tensor
 
@@ -128,15 +158,99 @@ def load_dataset(dataset_path):
 
     return HLANDataset(dataset)
 
+
+
+def train(model, device, data_loader, criterion, optimizer, epoch, print_freq=10):
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+    losses = AverageMeter()
+    
+    model.train()
+
+    end = time.time()
+    for i, (input, target) in enumerate(data_loader):
+        # measure data loading time
+        data_time.update(time.time() - end)
+        
+        if isinstance(input, tuple):
+            input = tuple([e.to(device) if type(e) == torch.Tensor else e for e in input])
+        else:
+            input = input.to(device)
+        target = target.to(device)
+
+        optimizer.zero_grad()
+        output = model(input)
+        loss = criterion(output, target)
+        assert not np.isnan(loss.item()), 'Model diverged with loss = NaN'
+        
+        loss.backward()
+        # TODO: gradient clipping, if needed: https://stackoverflow.com/questions/54716377/how-to-do-gradient-clipping-in-pytorch
+        optimizer.step()
+        
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+        
+        losses.update(loss.item(), target.size(0))
+        
+        if i % print_freq == 0:
+            print('Epoch: [{0}][{1}/{2}]\t'
+                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
+                  'Loss {loss.val:.4f} ({loss.avg:.4f})'.format(
+                      epoch, i, len(data_loader), batch_time=batch_time,
+                      data_time=data_time, loss=losses))
+            
+    return losses.avg
+
+
+def get_device():
+    return torch.device("cuda" if torch.cuda.is_available() and USE_CUDA else "cpu")
+
+
+def get_label_embeddings():
+    model = Word2Vec.load('../features/saved_embedding_models/code_embeddings.model')
+    with open(os.path.join(PROCESSED_DATA_PATH, 'code_map.p'), 'rb') as f:
+        code_map = pickle.load(f)
+    sorted_codes = sorted(code_map.items(), key=lambda x: x[1])
+    embeddings = np.array([model.wv[code[0]] for code in sorted_codes])
+    return embeddings.T
+    
+
 def run():
-    # train_dataset = load_dataset(os.path.join(PROCESSED_DATA_PATH, 'train_50.p'))
+    train_dataset = load_dataset(os.path.join(PROCESSED_DATA_PATH, 'train_50.p'))
     dev_dataset = load_dataset(os.path.join(PROCESSED_DATA_PATH, 'dev_50.p'))
-    # test_dataset = load_dataset(os.path.join(PROCESSED_DATA_PATH, 'test_50.p'))
+    test_dataset = load_dataset(os.path.join(PROCESSED_DATA_PATH, 'test_50.p'))
+    label_embeddings = get_label_embeddings()
 
-    batch = document_collate_function([dev_dataset[i] for i in range(8)])
+    train_loader = DataLoader(
+        dataset=train_dataset, batch_size=BATCH_SIZE,
+        shuffle=True, collate_fn=document_collate_function,
+        num_workers=0
+    )
+    dev_loader = DataLoader(
+        dataset=dev_dataset, batch_size=BATCH_SIZE,
+        shuffle=False, collate_fn=document_collate_function,
+        num_workers=0
+    )
+    test_loader = DataLoader(
+        dataset=test_dataset, batch_size=BATCH_SIZE,
+        shuffle=False, collate_fn=document_collate_function,
+        num_workers=0
+    )
 
-    model = HLAN()
-    model(batch[0])
+    model = HLAN(label_embeddings=label_embeddings)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters())
+    device = get_device()
+    torch.manual_seed(10)
+    model.to(device)
+    criterion.to(device)
+
+    for epoch in range(NUM_EPOCHS):
+        train(model, device, train_loader, criterion, optimizer, epoch)
+        torch.save(model, MODEL_OUTPUT_PATH)
+
 
 
 if __name__ == '__main__':
